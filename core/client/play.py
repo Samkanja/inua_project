8d518df184afc7c3035d06bb9d2db9bd52ee9259
Samkanja <samuelkanja4@gmail.com>
2024-05-29 07:15:19 -0700

    created path

--- core/client/play.py
+++ core/client/play.py
@@ -1,9 +1,19 @@
 import yaml
 from pathlib import Path
 
+ROOT = Path().absolute()
+PATH= ROOT / 'core/client'
+
+FILENAME = '_about.yaml'
+
+def read_file():
+    web_data =  yaml.load(open(PATH / FILENAME), Loader=yaml.FullLoader)
+    return web_data
 
 
 
+dic = read_file()
+print(dic)
     
 
 
--- core/client/routes.py
+++ core/client/routes.py
@@ -1,13 +1,12 @@
 import yaml
+from .play import read_file
 from flask import Blueprint, render_template
 
-def read_file():
-    web_data =  yaml.load(open('core\client\_about.yaml'), Loader=yaml.FullLoader)
-    return web_data
+
 
 client_views = Blueprint('client',__name__)
 
-
+web_data = read_file()
 
 @client_views.route("/")
 def home():
@@ -16,7 +15,7 @@ def home():
 
 @client_views.route("/about")
 def about_us():
-    web_data =  read_file()
+    
     return render_template('about.html', abouts=web_data)
 
 
