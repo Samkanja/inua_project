452d3b25cb6ac6f3a671c4d260fda4a21e4beeeb
Samkanja <samuelkanja4@gmail.com>
2024-07-13 22:20:39 -0700

    added about

--- core/client/routes.py
+++ core/client/routes.py
@@ -3,22 +3,16 @@ from .play import read_file
 from flask import Blueprint, render_template
 
 
-
-client_views = Blueprint('client',__name__)
+client_views = Blueprint("client", __name__)
 
 web_data = read_file()
 
+
 @client_views.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
 @client_views.route("/about")
 def about_us():
-    
-    return render_template('about.html', abouts=web_data)
-
-
-
-
-
+    return render_template("about.html", abouts=web_data)
